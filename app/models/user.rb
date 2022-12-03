9d8295aaed2303443ecd402705e4b96ebb221830
takunakashima <5002523tk@gmail.com>
2022-12-03 15:56:07 +0900

    モデル単体テストコードモデル単体テストコード記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+        
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX , message:'is invalid. Include both letters and numbers'}
   validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
   KATAKANA_REGEXP = /\A[\p{katakana}\u{30fc}]+\z/
   validates :first_name_kana, presence: true, format: { with: KATAKANA_REGEXP, message:'is invalid. Input full-width characters' }
   KATAKANA_REGEXP = /\A[\p{katakana}\u{30fc}]+\z/
